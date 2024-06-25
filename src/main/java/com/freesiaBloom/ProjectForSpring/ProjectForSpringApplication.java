2a26a0548456869a54ce3a9fadff8e0c7c0f8ba9
FreesiaBloom <agata.jasinska215@gmail.com>
2024-06-25 16:06:04 +0200

    setup spring boot

---
+++ src/main/java/com/freesiaBloom/ProjectForSpring/ProjectForSpringApplication.java
@@ -0,0 +1,13 @@
+package com.freesiaBloom.ProjectForSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freesiaBloom/ProjectForSpring/ProjectForSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freesiaBloom.ProjectForSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
