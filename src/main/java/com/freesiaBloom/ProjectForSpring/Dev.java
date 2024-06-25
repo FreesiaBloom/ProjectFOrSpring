b1b1127b8440efe8ff2c36513a201ac4f00462c8
FreesiaBloom <agata.jasinska215@gmail.com>
2024-06-25 16:28:54 +0200

    impolement dependency injection

---
+++ src/main/java/com/freesiaBloom/ProjectForSpring/Dev.java
@@ -0,0 +1,12 @@
+package com.freesiaBloom.ProjectForSpring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("working on project");
+    }
+
+}
--- src/main/java/com/freesiaBloom/ProjectForSpring/ProjectForSpringApplication.java
+++ src/main/java/com/freesiaBloom/ProjectForSpring/ProjectForSpringApplication.java
@@ -2,12 +2,17 @@ package com.freesiaBloom.ProjectForSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProjectForSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectForSpringApplication.class, args);
+		ApplicationContext context = SpringApplication.run(ProjectForSpringApplication.class, args);
+
+		Dev obj = context.getBean(Dev.class);
+
+		obj.build();
 	}
 
 }
